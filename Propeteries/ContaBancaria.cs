c31c5349ea9427cbfde4b1822ce7139932c3c88a
bruno <bruno@BRUNODESKTOP>
2024-05-13 21:36:40 -0300

    Add project files.

---
+++ Propeteries/ContaBancaria.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+class ContaBancaria
+{
+    public int NumeroConta { get; }
+    public string NomeTitular { get; }
+    public double Saldo { get; private set; }
+
+    public ContaBancaria(int numeroConta, string nomeTitular, double depositoInicial = 0.0)
+    {
+        NumeroConta = numeroConta;
+        NomeTitular = nomeTitular;
+        Saldo = depositoInicial;
+    }
+
+    public void Deposito(double valor)
+    {
+        Saldo += valor;
+        Console.WriteLine($"Depósito de R${valor.ToString("F2", CultureInfo.InvariantCulture)} realizado com sucesso!");
+    }
+
+    public void Saque(double valor)
+    {
+        if (valor + 5.0 > Saldo)
+        {
+            Console.WriteLine("Saldo insuficiente para saque!");
+        }
+        else
+        {
+            Saldo -= (valor + 5.0);
+            Console.WriteLine($"Saque de R${valor.ToString("F2", CultureInfo.InvariantCulture)} realizado com sucesso!");
+        }
+    }
+
+    public void MostrarSaldo()
+    {
+        Console.WriteLine($"Saldo atual da conta {NumeroConta}: R${Saldo.ToString("F2", CultureInfo.InvariantCulture)}");
+    }
+
+    public override string ToString()
+    {
+        return $"Número da Conta: {NumeroConta}, Titular: {NomeTitular}, Saldo: R${Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+    }
+}
---
+++ Propeteries/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe o número da conta:");
+        int numeroConta = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Informe o nome do titular:");
+        string nomeTitular = Console.ReadLine();
+
+        Console.WriteLine("Haverá depósito inicial? (s/n):");
+        char resposta = char.Parse(Console.ReadLine());
+
+        double depositoInicial = 0.0;
+        if (resposta == 's' || resposta == 'S')
+        {
+            Console.WriteLine("Informe o valor do depósito inicial:");
+            depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+
+        ContaBancaria conta = new ContaBancaria(numeroConta, nomeTitular, depositoInicial);
+
+        Console.WriteLine();
+        Console.WriteLine("Dados da Conta:");
+        Console.WriteLine(conta);
+
+        Console.WriteLine();
+        Console.WriteLine("Realize um depósito:");
+        double valorDeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        conta.Deposito(valorDeposito);
+
+        Console.WriteLine();
+        Console.WriteLine("Dados Atualizados da Conta:");
+        Console.WriteLine(conta);
+
+        Console.WriteLine();
+        Console.WriteLine("Realize um saque:");
+        double valorSaque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        conta.Saque(valorSaque);
+
+        Console.WriteLine();
+        Console.WriteLine("Dados Atualizados da Conta:");
+        Console.WriteLine(conta);
+    }
+}
